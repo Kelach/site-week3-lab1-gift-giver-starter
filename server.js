b10a955a8795d578a6da8fcc3306b98215c8ea2c
Kelechi <kelechi@utexas.edu>
2023-06-20 14:45:53 -0700

    added server routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = require("./app")
+const app = require("./app");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+app.get("/", (request, response) => {
+  response.send("Hellow world");
+});
 
 app.listen(port, () => {
   console.log(`🚀 Server listening at http://localhost:${port}`)
